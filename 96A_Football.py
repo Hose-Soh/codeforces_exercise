68988737f4f794354af263ba0ab16b0a0639da8a
Hose-Soh <sohrawardy.mhose@gmail.com>
2024-09-22 19:17:22 +0400

    add solution

--- 96A_Football.py
+++ 96A_Football.py
@@ -1,17 +1,15 @@
 player_string = input()
-counter = 0
+c = 1
 
 for i in range(len(player_string)-1):
-    # if player_string[i] == player_string[i+1]:
-    #     counter+=1
-    # else:
-    #     counter=0
-    print(player_string[i], player_string[i+1])
+    if player_string[i] == player_string[i+1]:
+        c+=1
+        if c>=7:
+            print("YES")
+            break
+    else:
+        c=1
 
-print(counter)
-if counter==7:
-    print("YES")
-    
-else:
+if c < 7:
     print("NO")
     
